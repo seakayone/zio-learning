6c045d9d06360e8a9099f6db8052edf5702f7f2e
Christian Kleinboelting <kleinboelting@signal7.de>
2022-07-19 04:51:25 +0200

    use only TodoService in TodoApp

--- src/main/scala/learnzio/todo/TodoApp.scala
+++ src/main/scala/learnzio/todo/TodoApp.scala
@@ -9,11 +9,11 @@ import java.util.UUID
 
 object TodoApp {
 
-  def apply(): Http[TodoRepo with TodoService, Throwable, Request, Response] =
+  def apply(): Http[TodoService, Throwable, Request, Response] =
     Http.collectZIO[Request] {
 
       case Method.GET -> !! / "todos" =>
-        TodoRepo.findAll().map(_.toJson).map(Response.json(_))
+        TodoService.findAll().map(_.toJson).map(Response.json(_))
 
       case req @ Method.POST -> !! / "todos" =>
         req.bodyAsString
@@ -22,18 +22,19 @@ object TodoApp {
             case Right(newTodo) =>
               TodoService
                 .createTodo(newTodo)
-                .map(created => Response.json(created.toJson))
+                .map(_.toJson)
+                .map(Response.json(_))
             case Left(value) =>
               ZIO.succeed(Response.text(value).setStatus(Status.BadRequest))
           }
 
       case Method.GET -> !! / "todos" / id =>
-        TodoRepo.find(id).map {
+        TodoService.find(id).map {
           case Some(todo) => Response.json(todo.toJson)
           case None       => Response.status(Status.NotFound)
         }
 
       case Method.DELETE -> !! / "todos" / id =>
-        TodoRepo.delete(id).map(_ => Response.ok)
+        TodoService.delete(id).as(Response.ok)
     }
 }
--- src/main/scala/learnzio/todo/TodoService.scala
+++ src/main/scala/learnzio/todo/TodoService.scala
@@ -7,9 +7,21 @@ import java.util.UUID
 
 trait TodoService {
   def createTodo(newTodo: NewTodo): Task[Todo]
+  def find(id: String): Task[Option[Todo]]
+  def findAll(): Task[Iterable[Todo]]
+  def delete(id: String): Task[Unit]
 }
 
 object TodoService {
   def createTodo(newTodo: NewTodo): ZIO[TodoService, Throwable, Todo] =
     ZIO.service[TodoService].flatMap(_.createTodo(newTodo))
+
+  def find(id: String): ZIO[TodoService, Throwable, Option[Todo]] =
+    ZIO.service[TodoService].flatMap(_.find(id))
+
+  def findAll(): ZIO[TodoService, Throwable, Iterable[Todo]] =
+    ZIO.service[TodoService].flatMap(_.findAll())
+
+  def delete(id: String): ZIO[TodoService, Throwable, Unit] =
+    ZIO.service[TodoService].flatMap(_.delete(id))
 }
